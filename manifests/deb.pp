2b222f37501295281a1902b6fd31a022a9365a0b
Piotr Radkowski <piotr.radkowski@uj.edu.pl>
2014-10-31 12:15:23 +0100

    Missing parameter

--- manifests/deb.pp
+++ manifests/deb.pp
@@ -1,4 +1,4 @@
-define deb ($url, $package = $title) {
+define deb ($url, $package = $title, $version) {
 	exec { "install-${package}-deb":
 		command => "wget '${url}' -qO /tmp/${package} && dpkg -i /tmp/${package} && rm /tmp/${package}",
 		unless  => "dpkg -l ${package} | grep ^ii | grep \"\\<${version}\\>\" 2>&1 > /dev/null",
